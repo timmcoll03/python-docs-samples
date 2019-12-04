0c1815a5f4da6371c7a51261b20c07cbf88b0aa3
timmcoll03 <51029752+timmcoll03@users.noreply.github.com>
2019-12-04 08:42:45 -0500

    Add files via upload

---
+++ vision/cloud-client/quickstart/timfirst.py
@@ -0,0 +1,37 @@
+def run_timfirst():
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    # [START vision_python_migration_import]
+    from google.cloud import vision
+    from google.cloud.vision import types
+
+     # [END vision_python_migration_import]
+
+    # Instantiates a client
+    # [START vision_python_migration_client]
+    client = vision.ImageAnnotatorClient()
+    # [END vision_python_migration_client]
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath('mepic.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+    # [END vision_quickstart]
+
+
+if __name__ == '__main__':
+    run_timfirst()
